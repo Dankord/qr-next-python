9e50fa8a54d7459ae8a3d5d12750ec83f9389c10
Heikouzen <phoneselfie23@gmail.com>
2025-01-22 00:33:02 +0800

    Yo this is a test okay?

--- backend/app.py
+++ backend/app.py
@@ -34,35 +34,7 @@ def url():
     
     return send_file(img_byte_arr, mimetype="image/png")
 
-@app.route('/api/qr/text', methods=['POST'])
-def text():
-    data = request.get_json()
-    
-    if not data:
-       return jsonify({'error': 'No data provided'}), 400
 
-    text = data.get('text')
-    color = data.get('color', 'black')
-    bgColor = data.get('bgColor', 'white')
-
-    if not text:
-        return jsonify({'error': 'Text not provided'}), 400
-    qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=3,
-    )
-    qr.add_data(text)
-    qr.make(fit=True)
-    
-    img = qr.make_image(fill_color=color, back_color=bgColor)
-    
-    img_byte_arr = io.BytesIO()
-    img.save(img_byte_arr, format='PNG')
-    img_byte_arr.seek(0)
-
-    return send_file(img_byte_arr, mimetype="image/png")
 
 if __name__ == "__main__":
     app.run(debug=True)
